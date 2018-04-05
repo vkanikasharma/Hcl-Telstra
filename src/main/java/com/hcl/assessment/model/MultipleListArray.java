832abad768d6e6ae212f5acf61edc3f1b928c8ed
vkanikasharma <38092329+vkanikasharma@users.noreply.github.com>
2018-04-05 19:12:25 +0530

    Create MultipleListArray.java

---
+++ src/main/java/com/hcl/assessment/model/MultipleListArray.java
@@ -0,0 +1,68 @@
+package com.hcl.assessment.model;
+
+public class MultipleListArray {
+	
+	private Integer[] Array1;
+	private Integer[] Array2;
+	private Integer[] Array3;
+	
+	
+	
+	public MultipleListArray() {}
+
+
+
+	/**
+	 * @return the array1
+	 */
+	public Integer[] getArray1() {
+		return Array1;
+	}
+
+
+
+	/**
+	 * @param array1 the array1 to set
+	 */
+	public void setArray1(Integer[] array1) {
+		Array1 = array1;
+	}
+
+
+
+	/**
+	 * @return the array2
+	 */
+	public Integer[] getArray2() {
+		return Array2;
+	}
+
+
+
+	/**
+	 * @param array2 the array2 to set
+	 */
+	public void setArray2(Integer[] array2) {
+		Array2 = array2;
+	}
+
+
+
+	/**
+	 * @return the array3
+	 */
+	public Integer[] getArray3() {
+		return Array3;
+	}
+
+
+
+	/**
+	 * @param array3 the array3 to set
+	 */
+	public void setArray3(Integer[] array3) {
+		Array3 = array3;
+	}
+
+	
+}
