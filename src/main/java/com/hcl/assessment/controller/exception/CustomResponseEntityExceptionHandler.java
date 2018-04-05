108c90a2f65b41e11f11736aadf874c4333e2650
vkanikasharma <38092329+vkanikasharma@users.noreply.github.com>
2018-04-05 19:11:41 +0530

    Add files via upload

---
+++ src/main/java/com/hcl/assessment/controller/exception/CustomResponseEntityExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.hcl.assessment.controller.exception;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+@ControllerAdvice
+@RestController
+public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	@ExceptionHandler(InvalidInputException.class)
+	  public final ResponseEntity<ErrorDetails> handleUserNotFoundException(InvalidInputException ex, WebRequest request) {
+	    ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(),
+	        request.getDescription(false));
+	    return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+	  }
+}
---
+++ src/main/java/com/hcl/assessment/controller/exception/InvalidInputException.java
@@ -0,0 +1,25 @@
+package com.hcl.assessment.controller.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class InvalidInputException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public InvalidInputException(){
+		super();
+	}
+	
+	public InvalidInputException(String message){
+		super();
+	}
+
+}
+
+
+
