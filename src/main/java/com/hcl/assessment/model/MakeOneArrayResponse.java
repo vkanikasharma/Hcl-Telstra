ceebd9bedce1ec48194d0d03ac96c6edbac9587e
vkanikasharma <38092329+vkanikasharma@users.noreply.github.com>
2018-04-05 19:13:37 +0530

    Add files via upload

---
+++ src/main/java/com/hcl/assessment/model/MakeOneArrayResponse.java
@@ -0,0 +1,22 @@
+package com.hcl.assessment.model;
+
+public class MakeOneArrayResponse {
+	
+	Integer[] Array;
+
+	/**
+	 * @return the array
+	 */
+	public Integer[] getArray() {
+		return Array;
+	}
+
+	/**
+	 * @param array the array to set
+	 */
+	public void setArray(Integer[] array) {
+		Array = array;
+	}
+	
+
+}
