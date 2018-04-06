1aae50c605752fdfef78db44b85653f91e6090c7
vkanikasharma <38092329+vkanikasharma@users.noreply.github.com>
2018-04-06 14:48:17 +0530

    Update MakeOneArrayUtils.java
    Corrected the logic

--- src/main/java/com/hcl/assessment/controller/utils/MakeOneArrayUtils.java
+++ src/main/java/com/hcl/assessment/controller/utils/MakeOneArrayUtils.java
@@ -16,35 +16,27 @@ public class MakeOneArrayUtils {
 	 */
 	
 	public static MakeOneArrayResponse createAndSortAllArrays(MultipleListArray multipleArrayList){
-		
-		List<Integer> result = new ArrayList<Integer>();
+		int size = multipleArrayList.getArray1().length+multipleArrayList.getArray2().length+multipleArrayList.getArray3().length;
+		List<Integer> result = new ArrayList<>();
+		Integer[] oneArray = new Integer[size];
 		MakeOneArrayResponse response = new MakeOneArrayResponse();
-		if(multipleArrayList!=null){
-			for(Integer n: multipleArrayList.getArray1()){
-				if(!result.contains(n)){//check to avoid duplicates
-					result.add(n);
-				}
+		
+		//Combine all arrays into oneArray
+		System.arraycopy(multipleArrayList.getArray1(), 0, oneArray, 0, multipleArrayList.getArray1().length);
+		System.arraycopy(multipleArrayList.getArray2(), 0, oneArray, multipleArrayList.getArray1().length, multipleArrayList.getArray2().length);
+		System.arraycopy(multipleArrayList.getArray3(), 0, oneArray, multipleArrayList.getArray1().length+multipleArrayList.getArray2().length, multipleArrayList.getArray3().length);
+		
+		for(Integer n:oneArray){
+			if(!result.contains(n)){//check to avoid duplicates
+				result.add(n);
 			}
 			
-			for(Integer n: multipleArrayList.getArray2()){
-				if(!result.contains(n)){//check to avoid duplicates
-					result.add(n);
-				}
-			}
-			for(Integer n: multipleArrayList.getArray3()){
-				if(!result.contains(n)){//check to avoid duplicates
-					result.add(n);
-				}
-			}
-			
-			Collections.sort(result);//sort the array in ascending order
-			
-			
-			response.setArray(result.toArray(new Integer[result.size()]));
 		}
+		Collections.sort(result);//sort the array in ascending order
 		
-		
+		response.setArray(result.toArray(new Integer[result.size()]));
 		return response;
+		
 	}
 	
 	
