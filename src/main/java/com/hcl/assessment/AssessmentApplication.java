aacca05f9776215bb104e4ed1c78cf25195f80b8
vkanikasharma <38092329+vkanikasharma@users.noreply.github.com>
2018-04-06 15:46:38 +0530

    Update AssessmentApplication.java
    Added class level comments

--- src/main/java/com/hcl/assessment/AssessmentApplication.java
+++ src/main/java/com/hcl/assessment/AssessmentApplication.java
@@ -3,9 +3,14 @@ package com.hcl.assessment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring boot application class
+ * @author kanika.sharma
+ *
+ */
 @SpringBootApplication
 public class AssessmentApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AssessmentApplication.class, args);
 	}
