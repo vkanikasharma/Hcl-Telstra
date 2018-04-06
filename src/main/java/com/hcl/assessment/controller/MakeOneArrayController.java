dd2eac937bc2f6d790eeeec4929922e82ea7cc08
vkanikasharma <38092329+vkanikasharma@users.noreply.github.com>
2018-04-06 15:49:25 +0530

    Update MakeOneArrayController.java
    Added class level comments.

--- src/main/java/com/hcl/assessment/controller/MakeOneArrayController.java
+++ src/main/java/com/hcl/assessment/controller/MakeOneArrayController.java
@@ -10,6 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.hcl.assessment.controller.utils.MakeOneArrayUtils;
 import com.hcl.assessment.model.MultipleListArray;
 
+/**
+ * Controller class for MakrOneArray api. It will expose a post api to accept arrays of numbers. 
+ * In response, all arrays will be combined and resultant array will be sorted with all 
+ * array values (without duplicates).
+ * @author kanika.sharma
+ *
+ */
 @RestController
 public class MakeOneArrayController {
 	
