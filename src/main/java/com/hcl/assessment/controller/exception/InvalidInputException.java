a1797eb7031a2dad7b8ddc9534840a5af46ae7af
vkanikasharma <38092329+vkanikasharma@users.noreply.github.com>
2018-04-06 15:59:58 +0530

    Update InvalidInputException.java
    Added class level comments

--- src/main/java/com/hcl/assessment/controller/exception/InvalidInputException.java
+++ src/main/java/com/hcl/assessment/controller/exception/InvalidInputException.java
@@ -2,7 +2,11 @@ package com.hcl.assessment.controller.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
+/**
+ * Custom exception class to handle invalid input including null and empty.
+ * @author kanika.sharma
+ *
+ */
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InvalidInputException extends RuntimeException{
 
