c45e511af99545663f3c8cba1a314db4432fa0b0
vkanikasharma <38092329+vkanikasharma@users.noreply.github.com>
2018-04-11 15:17:30 +0530

    Update MultipleListArray.java
    Added support for upto 10 arrays.

--- src/main/java/com/hcl/assessment/model/MultipleListArray.java
+++ src/main/java/com/hcl/assessment/model/MultipleListArray.java
@@ -1,6 +1,7 @@
 package com.hcl.assessment.model;
 /**
  * Model class received as request for MakeOneArray api. 
+ * Supports upto 10 arrays.
  * @author kanika.sharma
  *
  */
@@ -9,8 +10,141 @@ public class MultipleListArray {
 	private Integer[] array1;//first array
 	private Integer[] array2;//second array
 	private Integer[] array3;//third array
+	private Integer[] array4;
+	private Integer[] array5;
+	private Integer[] array6;
+	private Integer[] array7;
+	private Integer[] array8;
+	private Integer[] array9;
+	private Integer[] array10;
 	
 	
+	/**
+	 * @return the array4
+	 */
+	public Integer[] getArray4() {
+		return array4;
+	}
+
+
+
+	/**
+	 * @param array4 the array4 to set
+	 */
+	public void setArray4(Integer[] array4) {
+		this.array4 = array4;
+	}
+
+
+
+	/**
+	 * @return the array5
+	 */
+	public Integer[] getArray5() {
+		return array5;
+	}
+
+
+
+	/**
+	 * @param array5 the array5 to set
+	 */
+	public void setArray5(Integer[] array5) {
+		this.array5 = array5;
+	}
+
+
+
+	/**
+	 * @return the array6
+	 */
+	public Integer[] getArray6() {
+		return array6;
+	}
+
+
+
+	/**
+	 * @param array6 the array6 to set
+	 */
+	public void setArray6(Integer[] array6) {
+		this.array6 = array6;
+	}
+
+
+
+	/**
+	 * @return the array7
+	 */
+	public Integer[] getArray7() {
+		return array7;
+	}
+
+
+
+	/**
+	 * @param array7 the array7 to set
+	 */
+	public void setArray7(Integer[] array7) {
+		this.array7 = array7;
+	}
+
+
+
+	/**
+	 * @return the array8
+	 */
+	public Integer[] getArray8() {
+		return array8;
+	}
+
+
+
+	/**
+	 * @param array8 the array8 to set
+	 */
+	public void setArray8(Integer[] array8) {
+		this.array8 = array8;
+	}
+
+
+
+	/**
+	 * @return the array9
+	 */
+	public Integer[] getArray9() {
+		return array9;
+	}
+
+
+
+	/**
+	 * @param array9 the array9 to set
+	 */
+	public void setArray9(Integer[] array9) {
+		this.array9 = array9;
+	}
+
+
+
+	/**
+	 * @return the array10
+	 */
+	public Integer[] getArray10() {
+		return array10;
+	}
+
+
+
+	/**
+	 * @param array10 the array10 to set
+	 */
+	public void setArray10(Integer[] array10) {
+		this.array10 = array10;
+	}
+
+
+
 	/**
 	 * default constructor
 	 */
