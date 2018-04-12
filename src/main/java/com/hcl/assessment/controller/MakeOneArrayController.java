b058cd5e93959ed6d346593de86bafb77471652e
vkanikasharma <38092329+vkanikasharma@users.noreply.github.com>
2018-04-12 12:48:45 +0530

    Update MakeOneArrayController.java
    Content type set to return proper response

--- src/main/java/com/hcl/assessment/controller/MakeOneArrayController.java
+++ src/main/java/com/hcl/assessment/controller/MakeOneArrayController.java
@@ -1,7 +1,10 @@
 package com.hcl.assessment.controller;
 
+import java.nio.charset.Charset;
+
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,13 +22,17 @@ import com.hcl.assessment.model.MultipleListArray;
  */
 @RestController
 public class MakeOneArrayController {
-	
+	public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),                        
+            Charset.forName("utf8")                     
+            );
 	
 	@PostMapping("/api/makeonearray")
 	public ResponseEntity<Object> makeOneArray(@RequestBody MultipleListArray multipleList){
 		
 		
 		return ResponseEntity.status(HttpStatus.OK).cacheControl(CacheControl.noCache()).header("Pragma", "no-cache")
+				.contentType(APPLICATION_JSON_UTF8)
 				.body(MakeOneArrayUtils.createAndSortAllArrays(multipleList));
 	}
 	
