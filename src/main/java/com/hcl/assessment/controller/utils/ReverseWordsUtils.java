d9cbb9fd6f0281ccfc3db4eff78c2d5969dc219f
vkanikasharma <38092329+vkanikasharma@users.noreply.github.com>
2018-04-17 18:30:33 +0530

    Update ReverseWordsUtils.java
    Logging added

--- src/main/java/com/hcl/assessment/controller/utils/ReverseWordsUtils.java
+++ src/main/java/com/hcl/assessment/controller/utils/ReverseWordsUtils.java
@@ -2,13 +2,16 @@ package com.hcl.assessment.controller.utils;
 
 import java.util.regex.Pattern;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * This class will provide method to reverse the words in a given sentence. 
  * @author kanika.sharma
  *
  */
 public class ReverseWordsUtils {
-	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ReverseWordsUtils.class);
 	public static final String SPACE = " ";
 	
 	/**
@@ -19,11 +22,13 @@ public class ReverseWordsUtils {
 	}
 	
 	/**
-	 * This method will take a sentence as in[ut and reverse all the words in the sentence.
+	 * This method will take a sentence as input and reverse all the words in the sentence.
 	 * @param sentence
 	 * @return
 	 */
 	public static String reverseWords(String sentence){
+		LOGGER.info("Inside reverseWords()");
+		LOGGER.debug("Reverse words for sentence: "+sentence);
 		StringBuilder reversedSentence = new StringBuilder();
 		Pattern pattern = Pattern.compile("\\s");
 		
@@ -33,6 +38,7 @@ public class ReverseWordsUtils {
 			reversedSentence.append((new StringBuilder(word)).reverse().toString())
 			.append(SPACE);
 		}
+		LOGGER.debug("Reverse words output: "+reversedSentence.toString().trim());
 		return reversedSentence.toString().trim();
 	}
 	
