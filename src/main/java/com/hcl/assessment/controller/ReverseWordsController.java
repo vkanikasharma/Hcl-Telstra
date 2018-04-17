4e9075e3b9ef9e1152e9b6df471c744bda7a236d
vkanikasharma <38092329+vkanikasharma@users.noreply.github.com>
2018-04-17 18:35:15 +0530

    Update ReverseWordsController.java
    Swagger documentation added

--- src/main/java/com/hcl/assessment/controller/ReverseWordsController.java
+++ src/main/java/com/hcl/assessment/controller/ReverseWordsController.java
@@ -11,21 +11,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.hcl.assessment.controller.exception.InvalidInputException;
 import com.hcl.assessment.controller.utils.ReverseWordsUtils;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 /**
  * This class will expose a get api which will take a sentence as input.
  * In response, it will return the sentence with all words in reverse.
  * @author kanika.sharma
  *
  */
+@Api(value="ReverseWords", description="Operation to reverse words in a sentence")
 @RestController
 public class ReverseWordsController {
 	
-	
+	@ApiOperation(value = "Reverses the words of sentence", response = String.class)
+	@ApiResponses(value = {
+	        @ApiResponse(code = 200, message = "Operation Successful"),
+	        @ApiResponse(code = 400, message = "Invalid Input: Either null or empty string")
+	}
+	)
 	@GetMapping("/api/ReverseWords")
 	public ResponseEntity<Object> getReversedWords(@RequestParam String sentence){
 		
 		if(Objects.isNull(sentence) || sentence.length()==0){
-			throw new InvalidInputException("Invalid Input: Either Null or empty string");
+			throw new InvalidInputException("Invalid Input: Either null or empty string");
 			
 		}
 		return ResponseEntity.status(HttpStatus.OK).cacheControl(CacheControl.noCache()).header("Pragma", "no-cache")
