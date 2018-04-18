86e9a2a7b2919555ee76881a49bdef141959713d
vkanikasharma <38092329+vkanikasharma@users.noreply.github.com>
2018-04-18 16:02:20 +0530

    Update ReverseWordsController.java
    Added logging

--- src/main/java/com/hcl/assessment/controller/ReverseWordsController.java
+++ src/main/java/com/hcl/assessment/controller/ReverseWordsController.java
@@ -2,6 +2,8 @@ package com.hcl.assessment.controller;
 
 import java.util.Objects;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,9 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hcl.assessment.controller.exception.ErrorMessages;
 import com.hcl.assessment.controller.exception.InvalidInputException;
 import com.hcl.assessment.controller.utils.ReverseWordsUtils;
-
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -25,18 +27,20 @@ import io.swagger.annotations.ApiResponses;
 @Api(value="ReverseWords")
 @RestController
 public class ReverseWordsController {
-	
-	@ApiOperation(value = "Reverses the words of sentence", notes="Operation to reverse words in a sentence", response = String.class)
+	private static final Logger LOGGER = LoggerFactory.getLogger(ReverseWordsController.class);
+	private static org.slf4j.Marker marker;	
+	@ApiOperation(value = "Reverses the letters of each word in a sentence.", notes="Reverses the letters of each word in a sentence.", response = String.class)
 	@ApiResponses(value = {
-	        @ApiResponse(code = 200, message = "Operation Successful"),
+	        @ApiResponse(code = 200, message = "OK"),
 	        @ApiResponse(code = 400, message = "Invalid Input: Either null or empty string")
 	}
 	)
 	@GetMapping("/api/ReverseWords")
 	public ResponseEntity<Object> getReversedWords(@RequestParam String sentence){
-		
+		LOGGER.info(marker, "IN: ReverseWords Controller {}","");
 		if(Objects.isNull(sentence) || sentence.length()==0){
-			throw new InvalidInputException("Invalid Input: Either null or empty string");
+			LOGGER.error(ErrorMessages.REVERSE_WORDS_ERROR_EMPTY);
+			throw new InvalidInputException(ErrorMessages.REVERSE_WORDS_ERROR_EMPTY);
 			
 		}
 		return ResponseEntity.status(HttpStatus.OK).cacheControl(CacheControl.noCache()).header("Pragma", "no-cache")
